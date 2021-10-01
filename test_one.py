1c948550e2e19034e6146949f065ba57ad3c4bb9
TPeubey <thomas.peubey@gmail.com>
2021-10-01 11:36:59 +0200

    Update test_one.py

--- test_one.py
+++ test_one.py
@@ -10,7 +10,8 @@ def get_pizza_number() -> int:
 
 
 def get_number_slice_by_people(people: int, pizza: int) -> int:
-    return people // pizza
+    default_slice_by_pizza = 8
+    return pizza * default_slice_by_pizza // people
 
 
 class TestOne(unittest.TestCase):
@@ -22,6 +23,9 @@ class TestOne(unittest.TestCase):
         number_pizza = get_pizza_number()
         self.assertEqual(2, number_pizza)
 
-    def test_x(self):
+    def test_number_slice_by_people_with_2_pizza_8_people(self):
         number_slice_by_people = get_number_slice_by_people(people=8, pizza=2)
         self.assertEqual(2, number_slice_by_people)
+
+    def test_even(self):
+        
